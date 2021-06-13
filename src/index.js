78947be8813e4189f66086842d72d13be7ac71e1
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-06-13 16:12:47 +0900

    タスクの追加機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックススの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divを生成
+  const div = document.createElement("div");
+  //classに名前を付与
+  div.className = "list-row";
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
